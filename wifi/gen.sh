aa5883cb3d4de88bbdfa64d466a4a7bbdab949d2
Clayton Smith <argilo@gmail.com>
2024-08-21 15:53:33 -0400

    Update flags for wifi challenge

--- wifi/gen.sh
+++ wifi/gen.sh
@@ -8,7 +8,7 @@ convert \
   -gravity Center \
   -pointsize 50 \
   -font 'Ubuntu-Sans-Medium' \
-  label:'Part 1: flag\{b2e2b3877aad4108\}' \
+  label:'Part 1: flag\{fcf83c1e2b501dd4\}' \
   -rotate 270 \
   -extent x800 \
   -background White \
@@ -26,8 +26,8 @@ convert \
   -pointsize 10 \
   -fill '#181818' \
   -font 'Ubuntu-Sans-Medium' \
-  -annotate +925+205 'Part 2: flag\{b2e2b3877aad4108\}' \
-  -set comment 'Part 3: flag\{b2e2b3877aad4108\}' \
+  -annotate +925+205 'Part 2: flag\{a80ce321aab12038\}' \
+  -set comment 'Part 3: flag\{3aa46bc4c2541a31\}' \
   combined.png
 
 grcc wifi_phy_paint_hier.grc
