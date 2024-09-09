5536c0f5fac4a6d8c2541b1c8797e623b8172970
Clayton Smith <argilo@gmail.com>
2024-09-08 23:13:22 -0400

    Add prototype POCSAG & FLEX challenges

--- sigid/gen.sh
+++ sigid/gen.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+grcc pagerserver.grc
+./pagerserver.py --filename sigid3.sigmf-data --offset 37500 &
+pid=$!
+sleep 5
+echo pocsag1200 0 931862500 alpha 31337 `echo "flag{74d74b540b070f16ed30c607bfbe0905}" | xxd -ps -c 200` | nc -q 1 localhost 52001
+kill $pid
+
+grcc pagerserver.grc
+./pagerserver.py --filename sigid4.sigmf-data --offset 62500 &
+pid=$!
+sleep 5
+echo flex 0 931862500 alpha 424242 `echo "flag{0511696e30d8a1b3329db1bc8e5057e5}" | xxd -ps -c 200` | nc -q 1 localhost 52001
+kill $pid
+
 grcc m17_packet.grc
 echo -en "\x05flag{a591743aff8edb92425d9541bdcff8e3}\x00" | m17-packet-encode -S VE3IRR/W4 -D ALL -C 0 -n 40 -f -o packet_symbols.f32
 cat <(dd if=/dev/zero bs=4 count=192) packet_symbols.f32 <(dd if=/dev/zero bs=4 count=192) > packet_symbols_padded.f32
