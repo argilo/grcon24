0a071a4ed2e0e942c6a9235a9a78a49d3c096486
Clayton Smith <argilo@gmail.com>
2024-09-08 22:01:37 -0400

    Add flag to m17 packet challenge

--- sigid/gen.sh
+++ sigid/gen.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 grcc m17_packet.grc
-echo -en "\x05Testing M17 packet mode.\x00" | m17-packet-encode -S VE3IRR/W4 -D ALL -C 0 -n 26 -f -o packet_symbols.f32
+echo -en "\x05flag{a591743aff8edb92425d9541bdcff8e3}\x00" | m17-packet-encode -S VE3IRR/W4 -D ALL -C 0 -n 40 -f -o packet_symbols.f32
 cat <(dd if=/dev/zero bs=4 count=192) packet_symbols.f32 <(dd if=/dev/zero bs=4 count=192) > packet_symbols_padded.f32
 # cat packet_symbols.f32 | m17-packet-decode -c
 ./m17_packet.py
