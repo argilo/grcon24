128055a90e91e7bb16cb17eae89d6cae8752e708
Clayton Smith <argilo@gmail.com>
2024-09-11 23:50:40 -0400

    Add destination call sign to M17 packet challenge

--- sigid/gen.sh
+++ sigid/gen.sh
@@ -15,7 +15,7 @@ echo flex 0 929312500 alpha 424242 `echo "flag{0511696e30d8a1b3329db1bc8e5057e5}
 kill $pid
 
 grcc m17_packet.grc
-echo -en "\x05flag{a591743aff8edb92425d9541bdcff8e3}\x00" | m17-packet-encode -S VE3IRR/W4 -D ALL -C 0 -n 40 -f -o packet_symbols.f32
+echo -en "\x05flag{a591743aff8edb92425d9541bdcff8e3}\x00" | m17-packet-encode -S VE3IRR/W4 -D KA0XTT -C 0 -n 40 -f -o packet_symbols.f32
 cat <(dd if=/dev/zero bs=4 count=192) packet_symbols.f32 <(dd if=/dev/zero bs=4 count=192) > packet_symbols_padded.f32
 # cat packet_symbols.f32 | m17-packet-decode -c
 ./m17_packet.py
